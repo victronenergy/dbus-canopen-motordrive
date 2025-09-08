6b75b43f27f007c357d607935413855340164eae
Nils Citoleux <nils@mobi2go.com>
2025-09-08 11:14:49 +0200

    chore: upgrade sdk version

--- scripts/up.sh
+++ scripts/up.sh
@@ -4,7 +4,7 @@
 if [ ! -f "build/toolchain.sh" ]; then
     echo "toolchain not found, downloading..."
     mkdir -p build
-    wget https://updates.victronenergy.com/feeds/venus/release/sdk/venus-scarthgap-x86_64-arm-cortexa8hf-neon-toolchain-v3.63.sh -O build/toolchain.sh
+    wget https://updates.victronenergy.com/feeds/venus/release/sdk/venus-scarthgap-x86_64-arm-cortexa8hf-neon-toolchain-v3.65.sh -O build/toolchain.sh
 fi
 
 docker compose up -d --build
