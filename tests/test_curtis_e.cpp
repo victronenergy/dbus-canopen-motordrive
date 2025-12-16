6d025b80a5772e9354444d67ea5abe6823885ec4
Nils Citoleux <nils.citoleux@gmail.com>
2025-12-16 11:33:08 +0100

    tests(CurtisE): coverage for negative current

--- tests/test_curtis_e.cpp
+++ tests/test_curtis_e.cpp
@@ -525,3 +525,91 @@ TEST_F(CurtisETest, currentDeadband) {
               30.0F);
     EXPECT_EQ(nodes[0].device->motorDirection->variant.value.UN8, 2);
 }
+
+TEST_F(CurtisETest, currentNegative) {
+    VeRawCanMsg message;
+
+    EXPECT_EQ(nodes[0].connected, veFalse);
+    connectToNode(1);
+    canOpenTx();
+    this->canMsgReadQueue.push_back(
+        {.canId = 0x581,
+         .length = 8,
+         .mdata = {0x80, 0x08, 0x10, 0x00, 0x00, 0x00, 0x02, 0x06}});
+    canOpenRx();
+    canOpenTx();
+    this->canMsgReadQueue.push_back(
+        {.canId = 0x581,
+         .length = 8,
+         .mdata = {0x42, 0x18, 0x10, 0x01, 0x49, 0x43, 0x00, 0x00}});
+    canOpenRx();
+    canOpenTx();
+    this->canMsgReadQueue.push_back(
+        {.canId = 0x581,
+         .length = 8,
+         .mdata = {0x42, 0x64, 0x34, 0x00, 0x00, 0xFD, 0xBB, 0x00}});
+    canOpenRx();
+    canOpenTx();
+    this->canMsgReadQueue.push_back(
+        {.canId = 0x581,
+         .length = 8,
+         .mdata = {0x42, 0x18, 0x10, 0x04, 0x01, 0x00, 0x00, 0x00}});
+    canOpenRx();
+    EXPECT_EQ(nodes[0].connected, veTrue);
+
+    this->canMsgSentLog.clear();
+
+    nodes[0].device->driver->readRoutine(&nodes[0]);
+    EXPECT_EQ(listCount(canOpenState.pendingSdoRequests), 6);
+
+    std::vector<VeRawCanMsg> queue;
+    // Swap Motor Direction
+    queue.push_back(
+        {.canId = 0x581,
+         .length = 8,
+         .mdata = {0x42, 0x6C, 0x30, 0x00, 0x00, 0x00, 0x00, 0x00}});
+    // Battery Voltage
+    queue.push_back(
+        {.canId = 0x581,
+         .length = 8,
+         .mdata = {0x42, 0x4C, 0x32, 0x00, 0x20, 0x0D, 0x00, 0x00}});
+    // Battery Current
+    queue.push_back(
+        {.canId = 0x581,
+         .length = 8,
+         .mdata = {0x42, 0x9E, 0x35, 0x00, 0x9C, 0xFF, 0x00, 0x00}});
+    // Motor RPM
+    queue.push_back(
+        {.canId = 0x581,
+         .length = 8,
+         .mdata = {0x42, 0x07, 0x32, 0x00, 0xF4, 0x01, 0x00, 0x00}});
+    // Motor Temperature
+    queue.push_back(
+        {.canId = 0x581,
+         .length = 8,
+         .mdata = {0x42, 0x0B, 0x32, 0x00, 0xFA, 0x00, 0x00, 0x00}});
+    // Controller Temperature
+    queue.push_back(
+        {.canId = 0x581,
+         .length = 8,
+         .mdata = {0x42, 0x2A, 0x32, 0x00, 0x2C, 0x01, 0x00, 0x00}});
+
+    while (!queue.empty()) {
+        this->canMsgReadQueue.push_back(queue.front());
+        queue.erase(queue.begin());
+        canOpenTx();
+        canOpenRx();
+    }
+
+    EXPECT_EQ(this->canMsgSentLog.size(), 6);
+    EXPECT_EQ(canOpenState.pendingSdoRequests->first, nullptr);
+
+    EXPECT_EQ(nodes[0].device->voltage->variant.value.Float, 52.5F);
+    EXPECT_EQ(nodes[0].device->current->variant.value.Float, -10.0F);
+    EXPECT_EQ(nodes[0].device->power->variant.value.SN32, -525);
+    EXPECT_EQ(nodes[0].device->motorRpm->variant.value.UN16, 500);
+    EXPECT_EQ(nodes[0].device->motorTemperature->variant.value.Float, 25.0F);
+    EXPECT_EQ(nodes[0].device->controllerTemperature->variant.value.Float,
+              30.0F);
+    EXPECT_EQ(nodes[0].device->motorDirection->variant.value.UN8, 2);
+}
